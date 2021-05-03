bea0305769047ecf385c704235496b031b2f088b
dfldiego <dfl.diego92@gmail.com>
2021-05-03 20:45:55 -0300

    redux-app. bootstrap. comunicacion padre e hijo.

---
+++ 02-redux-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HijoComponent } from './contador/hijo/hijo.component';
+import { NietoComponent } from './contador/nieto/nieto.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HijoComponent,
+    NietoComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
