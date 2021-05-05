2a2c5d95c222c03f84c8f564963af45616865929
dfldiego <dfl.diego92@gmail.com>
2021-05-05 18:56:34 -0300

    redux-app. comp hijo(multiplicar y dividir) action con argumento.

--- 02-redux-app/src/app/contador/contador.actions.ts
+++ 02-redux-app/src/app/contador/contador.actions.ts
@@ -1,4 +1,15 @@
-import { createAction } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
 
 export const incrementar = createAction('[Contador] Incrementar');
 export const decrementar = createAction('[Contador] Decrementar');
+
+// action recibe un argumento de tipo number.
+export const multiplicar = createAction(
+    '[Contador] Multiplicar',
+    props<{ numero: number }>()
+);
+
+export const dividir = createAction(
+    '[Contador] Dividir',
+    props<{ numero: number }>()
+);
--- 02-redux-app/src/app/contador/hijo/hijo.component.ts
+++ 02-redux-app/src/app/contador/hijo/hijo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.reducers';
-
+import * as actions from '../contador.actions';
 
 @Component({
   selector: 'app-hijo',
@@ -21,13 +21,11 @@ export class HijoComponent implements OnInit {
   }
 
   multiplicar() {
-    /* this.contador *= 2;
-    this.cambioContador.emit(this.contador); */
+    this.store.dispatch(actions.multiplicar({ numero: 2 }));
   }
 
   dividir() {
-    /* this.contador /= 2;
-    this.cambioContador.emit(this.contador); */
+    this.store.dispatch(actions.dividir({ numero: 2 }));
   }
 
 }
