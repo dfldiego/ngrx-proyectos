68b579e0aee399bfeb557a0f868ff7b9aacf47ef
dfldiego <dfl.diego92@gmail.com>
2021-05-08 16:13:23 -0300

    todoapp. toggle action. completar o desmarcar un TODO.

--- 03-todoapp/src/app/todos/todo-item/todo-item.component.ts
+++ 03-todoapp/src/app/todos/todo-item/todo-item.component.ts
@@ -1,6 +1,9 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Todo } from '../models/todo.model';
 import { FormControl, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import * as actions from '../todo.actions';
 
 @Component({
   selector: 'app-todo-item',
@@ -15,11 +18,21 @@ export class TodoItemComponent implements OnInit {
   txtInput: FormControl;
   editando: boolean = false;
 
-  constructor() { }
+  constructor(
+    private store: Store<AppState>
+  ) { }
 
   ngOnInit(): void {
     this.chkCompletado = new FormControl(this.todo.completado);
     this.txtInput = new FormControl(this.todo.texto, Validators.required);
+
+    /*
+      valueChanges -> evalua los cambios del checkbox y devuelve true o false.
+    */
+    this.chkCompletado.valueChanges.subscribe(valor => {
+      this.store.dispatch(actions.toggle({ id: this.todo.id }));
+    });
+
   }
 
   editar() {
--- 03-todoapp/src/app/todos/todo.actions.ts
+++ 03-todoapp/src/app/todos/todo.actions.ts
@@ -4,3 +4,8 @@ export const crear = createAction(
     '[Todo] Crear TODO',
     props<{ texto: string }>()
 );
+
+export const toggle = createAction(
+    '[Todo] Toggle TODO',
+    props<{ id: number }>()
+);
