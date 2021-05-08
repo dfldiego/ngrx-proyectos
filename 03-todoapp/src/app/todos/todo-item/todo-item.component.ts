ad2fd008246db74d0c8b6bc0e53ff3635b6ae14f
dfldiego <dfl.diego92@gmail.com>
2021-05-08 15:12:36 -0300

    todoapp. controlando todo-item focus() select() blur() @ViewChild

--- 03-todoapp/src/app/todos/todo-item/todo-item.component.ts
+++ 03-todoapp/src/app/todos/todo-item/todo-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Todo } from '../models/todo.model';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoItemComponent implements OnInit {
 
+  @Input() todo: Todo;
+  @ViewChild('inputFisico') txtInputFisico: ElementRef;
+  chkCompletado: FormControl;
+  txtInput: FormControl;
+  editando: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.chkCompletado = new FormControl(this.todo.completado);
+    this.txtInput = new FormControl(this.todo.texto, Validators.required);
+  }
+
+  editar() {
+    this.editando = true;
+
+    setTimeout(() => {
+      this.txtInputFisico.nativeElement.select();
+    }, 1);
+  }
+
+  terminarEdicion() {
+    this.editando = false;
   }
 
 }
